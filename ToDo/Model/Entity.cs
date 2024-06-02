1b14f8bed1895340a818fb9f31b2c854bd029663
Armah <77860336+armah001@users.noreply.github.com>
2024-06-02 13:42:43 +0000

    Initial commit

---
+++ ToDo/Model/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Model;
+
+public class Entity
+{
+    [Key]
+
+    public int Id { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
+
